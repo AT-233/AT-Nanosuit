b70b858f18f7d6400f40a13441aed022179ed6c3
Victor Wong <1606427136@qq.com>
2023-05-09 14:19:43 +0800

    Update

---
+++ Broadcast/NanoBroadcast.cs
@@ -0,0 +1,87 @@
+using System.Reflection;
+using Aki.Reflection.Patching;
+using EFT;
+using UnityEngine;
+
+namespace nanosuit
+{
+    public class NanoBroadcast : MonoBehaviour
+    {
+        public AudioClip[] Bt7274Audio;
+        public static bool killone;
+        public static bool isready;
+        private int voicechoose = 0;
+        private float readytime = 0;
+        private bool isbroad;
+        // Start is called before the first frame update
+        void Start()
+        {
+            isbroad = true;            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (nanosuitcore.BroadcastOnline.Value && isbroad)
+            {
+                new NanoSystemPatch().Enable();
+                voicechoose = 0;
+                killone = false;
+                isready = true;
+                isbroad = false;
+                readytime = 0;
+            }
+            if (!nanosuitcore.BroadcastOnline.Value && !isbroad)
+            {
+                new NanoSystemPatch().Disable();
+                voicechoose = 0;
+                killone = false;
+                isready = true;
+                isbroad = true;
+                readytime = 0;
+            }
+            if (killone && isready)
+            {
+                this.GetComponent<AudioSource>().volume = nanosuitcore.nanovolume.Value;
+                this.GetComponent<AudioSource>().clip = Bt7274Audio[voicechoose];
+                this.GetComponent<AudioSource>().Play();
+                killone = false;
+                isready = false;
+                voicechoose ++;
+                if (voicechoose == 4)
+                { 
+                    voicechoose = 0; 
+                }
+            }
+            if (killone && !isready)
+            {
+                killone = false;
+            }
+            if (!isready)
+            {
+                readytime += Time.deltaTime;
+                if(readytime > 3)
+                {
+                    isready = true;
+                    readytime = 0;
+                }
+            }
+        }      
+    }
+    public class NanoSystemPatch : ModulePatch
+    {
+        //搜索EFT空间下的Player类里的OnBeenKilledByAggressor
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethod("OnBeenKilledByAggressor", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+        [PatchPostfix]
+        private static void PatchPostFix(ref Player __instance, Player aggressor)
+        {
+            if (aggressor.IsYourPlayer)
+            {
+                NanoBroadcast.killone = true;
+            }
+        }
+    }
+}
---
+++ minimap/NanoMimiradarplayer.cs
@@ -0,0 +1,26 @@
+using System;
+using EFT;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace nanosuit
+{
+    public class NanoMimiradarplayer : MonoBehaviour
+    {
+        private GameObject player;
+        // Start is called before the first frame update
+        void Start()
+        {            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (player == null)
+            {
+                player = GameObject.Find("Nanohands");//ÕÒµ½Íæ¼Ò
+            }
+            this.GetComponent<RectTransform>().eulerAngles = new Vector3(0, 0, -player.transform.eulerAngles.y+180);
+        }
+    }
+}
